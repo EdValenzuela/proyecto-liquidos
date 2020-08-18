03453bc893b355182f01abffd7dd6f6a5c081dbc
Eduardo Valenzuela <e.valenzuelap@alumnos.duoc.cl>
2020-08-18 12:57:56 -0400

    Creación del gitpages

---
+++ docs/precache-manifest.6ff5a8b7d14804cac85b24c99ef14772.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8185ae647e1f3239d1be5eba501235ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa632a6f65c6545acd4",
+    "url": "/static/css/main.d1b1d5fc.chunk.css"
+  },
+  {
+    "revision": "b5da6bc14dfba06c3521",
+    "url": "/static/js/2.a6385393.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a6385393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa632a6f65c6545acd4",
+    "url": "/static/js/main.190dc030.chunk.js"
+  },
+  {
+    "revision": "71600757e3bb1530ce07",
+    "url": "/static/js/runtime-main.5a7600ea.js"
+  },
+  {
+    "revision": "85774594d53a30611ff08617a8449dc7",
+    "url": "/static/media/logo.85774594.png"
+  }
+]);
